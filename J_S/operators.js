15275fc67bd68831f71b1c3bd3043dccebad8050
Pars41 <ysfayds41@gmail.com>
2022-12-12 15:50:35 +0300

    comparison

--- J_S/operators.js
+++ J_S/operators.js
@@ -1,14 +1,36 @@
 let result;
-let a = 10, b = 20, c = 32;
+let a = 10, b =20, c = 32;
+
 // Arithmetic Operators
+
 result = a + b;
 result = a - b;
 result = a * b;
 result = a / b;
 result = c % a;
+result = a++;
+result = ++a;
+result = --a;
+result = a--;
+
+
 // Assignment Operators
 
+result = a;
+result += a;
+result -= a;
+result *= a;
+result /= a;
+result %= a;
+
 // Comparison Operators 
+
+result = (a == b);
+result = (a != b);
+result = (a === b);
+result = (a > b);
+result = (a < b);
+result = (a <= b);
 
 // Logical Operators
 
