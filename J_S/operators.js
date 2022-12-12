2a97fcd23ff61723122efca3dff9b625bcdd27c3
Pars41 <ysfayds41@gmail.com>
2022-12-12 15:18:04 +0300

    types of operators

---
+++ J_S/operators.js
@@ -0,0 +1,15 @@
+let result;
+let a = 10, b = 20, c = 32;
+// Arithmetic Operators
+result = a + b;
+result = a - b;
+result = a * b;
+result = a / b;
+result = c % a;
+// Assignment Operators
+
+// Comparison Operators 
+
+// Logical Operators
+
+console.log(result);
