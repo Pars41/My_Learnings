0b2f092708ec761e051fcd376dbff1bab51a7d5a
Pars41 <ysfayds41@gmail.com>
2022-12-13 18:28:12 +0300

    aa

--- J_S/if-else_practice.js
+++ J_S/if-else_practice.js
@@ -1,40 +1,40 @@
 // Bir sayının 41-139 arasında olup olmadığını kontrol ediniz
 
-// let sayi ;
-// sayi = 40;
-// if(sayi>41 && sayi<139){
-//     console.log("evet sayı 41 ila 139 arasındadır");
-// }else{
-//     console.log("sayı belirtilen aralık içerisinde değildir");
-// }
+let sayi ;
+sayi = 40;
+if(sayi>41 && sayi<139){
+    console.log("evet sayı 41 ila 139 arasındadır");
+}else{
+    console.log("sayı belirtilen aralık içerisinde değildir");
+}
 
 // Bir sayının pozitif tek sayı olup olmadığını kontrol ediniz
 
-// let sayi1;
-// sayi1 = 86;
-//  if(sayi1>0 && sayi1%2==1){
-//     console.log("evet sayı pozitif tek sayıdır");
-//  }else{
-//     console.log("hayır sayı pozitif tek sayı değildir");
-//  }
+let sayi1;
+sayi1 = 86;
+ if(sayi1>0 && sayi1%2==1){
+    console.log("evet sayı pozitif tek sayıdır");
+ }else{
+    console.log("hayır sayı pozitif tek sayı değildir");
+ }
 
 // x, y, z sayılarının büyüklük karşılaştırmasını yapınız (else if i araştırınız)
 
-// let x, y, z ;
-// x = 151;
-// y = 151;
-// z = 151;
-// if(x>y && x>z){
-//     console.log("x en büyük sayıdır");
-// }
-// else if(y>x && y>z){
-//     console.log("y en büyük sayıdır");
-// }
-// else if (z>x && z>y){
-//     console.log("z en büyük sayıdır");
-// }else{
-//     console.log("sayılar eşittir");
-// }
+let x, y, z ;
+x = 151;
+y = 151;
+z = 151;
+if(x>y && x>z){
+    console.log("x en büyük sayıdır");
+}
+else if(y>x && y>z){
+    console.log("y en büyük sayıdır");
+}
+else if (z>x && z>y){
+    console.log("z en büyük sayıdır");
+}else{
+    console.log("sayılar eşittir");
+}
 
 // 2 vize (%40) ve 1 final (%60) notuna göre hesaplanan ortalama için;
     // a- Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazsın.
