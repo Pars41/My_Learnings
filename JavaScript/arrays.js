ec5767d75d3c1803a209b21e6141a97ad129cd59
Pars41 <ysfayds41@gmail.com>
2022-12-16 12:22:55 +0300

    arrays

---
+++ JavaScript/arrays.js
@@ -0,0 +1,30 @@
+let urun1 = []
+urun1[0]= "lenova";
+urun1[1]= 28000;
+urun1[2]="space gray";
+
+let urun2 = []
+urun2[0]= "monster";
+urun2[1]= 35000;
+urun2[2]="light purple";
+
+
+let urun3 = []
+urun3[0]= "asus";
+urun3[1]= 24000;
+urun3[2]=["dark black","white","yellow"];
+
+
+
+
+
+
+
+
+
+console.log(urun1);
+console.log(urun2);
+console.log(urun3);
+
+console.log(urun3[1].toString());
+console.log(urun3[2][2]);
