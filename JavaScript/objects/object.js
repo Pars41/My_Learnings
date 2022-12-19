26dee07499637161042be37f95b34d6888782360
Pars41 <ysfayds41@gmail.com>
2022-12-19 14:06:27 +0300

    objects

--- JavaScript/objects/object.js
+++ JavaScript/objects/object.js
@@ -2,7 +2,30 @@ let ogrenci1 = {
     "ad":"yusuf",
     "soyad":"aydos",
     "yas": 30,
-    "şehir": "ankara"
+    "şehir": "ankara",
+    "hobi": ["sinema","spor","music"],
+    "okullar": {
+        "ilkokul": "Atatürk iöo",
+        "ortaokul": "Kemal paşa oo",
+        "lise": "Kuleli Askeri Lisesi",
+        "üniversite": "kho"
+    }
 }
 
-console.log(ogrenci1);
+let ogrenci2 = {
+    "ad":"hulki",
+    "soyad":"sev",
+    "yas": 35,
+    "şehir": "uşak",
+    "hobi": ["oyun","kitap","music"],
+    "okullar": {
+        "ilkokul": "inönü iöo",
+        "ortaokul": "rüstem paşa oo",
+        "lise": "asd meslek Lisesi",
+        "üniversite": "SADÜ"
+    }
+}
+
+
+console.log(ogrenci1.okullar.lise.toUpperCase());
+console.log(ogrenci2.okullar.üniversite.toLowerCase());
