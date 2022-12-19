b05797c39d07cf7c91986364b89800c449cf0d5f
Pars41 <ysfayds41@gmail.com>
2022-12-19 16:50:13 +0300

    loops-practice

--- JavaScript/loops/loops_practice.js
+++ JavaScript/loops/loops_practice.js
@@ -35,15 +35,36 @@ let urunler = ["iphone 12","samsung s22","iphone 13","samsung s23","samsung s20"
 // }
 
 // 5- urunler listesinde samsung geçen kaç ürün vardır?
-let adet = 0
-for(urun of urunler){
-    if(urun.includes("samsung")){
-        adet++
-        console.log(urun);
-    }
-}
-console.log(adet);
+// let adet = 0
+// for(urun of urunler){
+//     if(urun.includes("samsung")){
+//         adet++
+//         console.log(urun);
+//     }
+// }
+// console.log(adet);
 
-for(urun of urunler) {
-    urun.includes("samsung") ? console.log(urun) : console.log("no");
+// for(urun of urunler) {
+//     urun.includes("samsung") ? console.log(urun) : console.log("no");
+// }
+
+let ogrenciler = [
+    {"ad":"yusuf", "soyad": "aydos", "notlar": [90,70,90]},
+    {"ad":"cemşit", "soyad": "sakar", "notlar": [80,70,80]},
+    {"ad":"hulki", "soyad": "önder", "notlar": [10,20,60]}
+];
+
+// ogrenciler listesindeki her öğrencinin not ortalaması ve başarı durumlarını yazdırınız.
+
+for(let i = 0 ; i<ogrenciler.length;i++){
+  console.log(`${ogrenciler[i].ad}un not ortalaması: ${Math.ceil((ogrenciler[i].notlar[0]+ogrenciler[i].notlar[1]+ogrenciler[i].notlar[2])/3)} dir`); 
+  (Math.ceil((ogrenciler[i].notlar[0]+ogrenciler[i].notlar[1]+ogrenciler[i].notlar[2])/3))>=50 ? console.log(`${ogrenciler[i].ad} başarılıdır`):console.log(`${ogrenciler[i].ad} sınıfta kaldı`)
 }
+
+// tüm öğrencilerin not ortalaması kaçtır?
+let totalogr=0;
+for(let i = 0 ; i<ogrenciler.length;i++){
+totalogr+=Math.ceil((ogrenciler[i].notlar[0]+ogrenciler[i].notlar[1]+ogrenciler[i].notlar[2])/3)
+
+}
+console.log("tüm öğrencilerin not ortalaması: ",Math.round(totalogr/3),"dir");
