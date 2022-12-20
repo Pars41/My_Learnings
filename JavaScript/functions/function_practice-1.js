6e481c3b47cbe9cd105d109a734ea9e6467dead4
Pars41 <ysfayds41@gmail.com>
2022-12-20 13:26:52 +0300

    func prac

---
+++ JavaScript/functions/function_practice-1.js
@@ -0,0 +1,16 @@
+function ucKurusaBesKofte(kurus,kacKöfte){
+    let birKofte = 15;
+    let hakEdis =Math.floor(kurus/birKofte);
+    let hakEdilenTutar=hakEdis*birKofte;
+    let talepTutar =kacKöfte*birKofte
+
+        if(((talepTutar-hakEdilenTutar)-kurus%birKofte)>0){
+            console.log(`${hakEdis} köfte yiyebilirsiniz,${kacKöfte} adet köfte yemek isterseniz ${(talepTutar-hakEdilenTutar)-kurus%birKofte} lira daha ödeme yapmalısınız!`); 
+        }else{
+            console.log(`${hakEdis} köfte yiyebilirsiniz, ${Math.abs((talepTutar-hakEdilenTutar)-kurus%birKofte)} lira buyrun para üstünüz!`); 
+        }
+
+    
+}
+
+ucKurusaBesKofte(250,10);
