e6c6fe1f51a22d87696ebdc3868f7586308b63e3
Pars41 <ysfayds41@gmail.com>
2022-12-20 12:45:50 +0300

    retired

--- JavaScript/functions/functions.js
+++ JavaScript/functions/functions.js
@@ -1,7 +1,24 @@
-function yasHesapla(dogumYılı,isim){
-    let yas = new Date().getFullYear()-dogumYılı
-    console.log(`${isim}, ${yas} yaşındasınız!`);
+// function yasHesapla(dogumYılı,isim){
+//     let yas = new Date().getFullYear()-dogumYılı
+//     console.log(`${isim}, ${yas} yaşındasınız!`);
+// }
+
+
+// yasHesapla(1992,"Yusuf");
+
+function yasHesapla(dogumYılı){
+    return new Date().getFullYear()-dogumYılı
+    
 }
 
 
-yasHesapla(1992,"Yusuf");
+
+function emeklilikKalan(isim,dogumYılı){
+    let kalan = 65-yasHesapla(dogumYılı)
+    kalan>0 ? console.log(`${isim},emekliliğe ${kalan} yıl kaldı!`) : console.log(`${isim}, zaten emekli oldunuz!`);
+
+}
+
+emeklilikKalan("Cemal",1992);
+emeklilikKalan("Ragıp",1971);
+emeklilikKalan("Hasan",1941);
