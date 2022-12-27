2036c88fef8f10a2491fa19d3f1cc33fb5dcc264
Pars41 <ysfayds41@gmail.com>
2022-12-27 15:16:10 +0300

    faktoryel

--- JavaScript/arrow function/arrow.js
+++ JavaScript/arrow function/arrow.js
@@ -1,10 +1,22 @@
 // ES6 => JS
 
-// const topla = (a,b) => a+b;
+// const topla = (a,b) => {
+//     sonuc = a+b;
+//     console.log(sonuc);
+    
+// }
+
+// topla(41,5);
+
 
 // console.log(topla(5,4));
 
-// const yasHesapla = (dogumyılı) => 2022 -dogumyılı;
+// const yasHesapla = (dogumyılı) => { 
+//     return 2022 -dogumyılı;
+
+// } 
+
+
 // console.log(yasHesapla(1992));
 
 // Void function (boş fonksiyon / birşey döndürmez)
@@ -20,11 +32,13 @@
 // console.log(volume(5,12).toFixed(3));
 // console.log(volume(5,12).toPrecision(7));
 // console.log(volume(5,12).toExponential(5));
+// console.log(volume(5,12));
 
 // Example ; Yaş Hesaplama;
 
 // const calculateAge = (dgmyıl) => new Date().getFullYear() - dgmyıl;
 // console.log(calculateAge(1992));
+// console.log(new Date());
 
 // const fibo = n => {
 //     let fib1 = 1,fib2 = 1,sum = 0;
@@ -41,9 +55,52 @@
 //     }
     
 // }
-// fibo(1);
+// fibo(7);
 
 
 // RECURSION FUNCTION
 // Fonksiyonun kendisini çağırmasına recursiflik denir
+// FAKTORYEL döngü kullanarak
+// const faktoryel = n => {
+//     if(n<0){
+//         console.log("Satı 0 veya daha büyük bir sayı olmalıdır");
+//     }else if(n === 0 || n === 1){
+//         console.log(`${n}! = 1`);
+//     }else{
+//         let sum = 1;
 
+//         for(let i = 1; n>=i ; i++){
+//             sum = sum*i
+//         }
+//         console.log(`${n}! = ${sum}`);
+
+//     }
+// }
+// faktoryel(4);
+
+// const faktoryel = n => {
+//     if(n<0){
+//         return "Sayı 0 veya daha büyük bir sayı olmalıdır";
+//     }else if(n === 0 || n === 1){
+//         return 1;
+//     }else{
+//          return n* faktoryel(n-1);
+//     }
+// }
+// console.log(faktoryel(3));
+
+
+const faktoryel = n => {
+    if(n<0){
+        console.log("Sayı 0 veya daha büyük bir sayı olmalıdır"); 
+    }
+    else if(n === 0 || n === 1)
+    {
+        return 1; 
+    }
+    else
+    {
+         return n* faktoryel(n-1);
+    }
+}
+console.log(faktoryel(4));
