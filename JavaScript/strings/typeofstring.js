27f9f4a1f6021b46f1de539474d44802a2e69c7e
Pars41 <ysfayds41@gmail.com>
2022-12-28 21:17:22 +0300

    string

--- JavaScript/strings/typeofstring.js
+++ JavaScript/strings/typeofstring.js
@@ -9,12 +9,21 @@ let str4 = new String("This is a string")
 
 
 
-console.log(str1);
-console.log(str2);
-console.log(str3);
-console.log(str4);
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
 
-console.log(typeof str1);
-console.log(typeof str2);
-console.log(typeof str3);
-console.log(typeof str4);
+console.log(typeof str1,"--->",str1);
+console.log(typeof str2,"--->",str2);
+console.log(typeof str3,"--->",str3);
+console.log(typeof str4,"--->",str4);
+
+let str5 = "Portakallar çiçek açmış";
+
+console.log(str5[0],str5[1],str5[2],str5[3],str5[4],str5[5],str5[6],str5[7],str5[8],str5[9],str5[10]);
+console.log(str5[12],str5[13],str5[14],str5[15],str5[16]);
+console.log(str5[18],str5[19],str5[20],str5[21],str5[22]);
+
+
+console.log(str5[str5.length-1]);
