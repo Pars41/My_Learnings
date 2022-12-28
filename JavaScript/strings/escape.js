edaf5e26a0c6db4af96f95a8edd61160eeb146a2
Pars41 <ysfayds41@gmail.com>
2022-12-28 21:26:49 +0300

    escape sequences

---
+++ JavaScript/strings/escape.js
@@ -0,0 +1,8 @@
+// ESCAPE SEQUENCES///
+
+let sonuc = "";
+sonuc = "İncirler olana\n kadar gel"
+sonuc = "kaldırımlar \t çok dar"
+sonuc = "balinalar\0hayli zayıf"
+sonuc = "havalar orda soğuk mu \\ sıcak  mı?"
+console.log(sonuc);
