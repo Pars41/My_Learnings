1010b0495385280c1461646c78a0317a73820a83
Pars41 <ysfayds41@gmail.com>
2022-12-28 20:24:53 +0300

    strings

---
+++ JavaScript/strings/typeofstring.js
@@ -0,0 +1,20 @@
+// STRING METHODS \\\\\\\\\\\\\\\\
+console.log("********String Methods**********");
+
+let str1 = "This is a string"
+let str2 = 'This is a string'
+let str3 = `This is a string`
+let str4 = new String("This is a string")
+
+
+
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(typeof str3);
+console.log(typeof str4);
