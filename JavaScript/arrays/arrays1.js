51ea0544226d2778e6e6bad501680baae9bd3a63
Pars41 <ysfayds41@gmail.com>
2023-01-02 21:36:52 +0300

    arrays

--- JavaScript/arrays/arrays1.js
+++ JavaScript/arrays/arrays1.js
@@ -2,9 +2,13 @@
 
 //1.Yöntem (Array literal) - Tercih edilen yöntem
 
-const names = ["samet","can","hüsrev","levent"];
+const names = ["Ahmet","Furkan","Zeynep","Yusuf"];
 console.log(names.length); 
 console.log(typeof names);
+names[3] = names[3].toUpperCase()
+console.log(names);
+
+console.log(names.at(-1));   //son eleman -1 index
 
 //2.Yöntem (Object Constructor)
 
@@ -14,3 +18,5 @@ console.log(codingLangs);
 //3.Yöntem (Array.of())
 const arr = Array.of(3)
 console.log(arr);
+
+
