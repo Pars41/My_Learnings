9f3868857dc04bd3ee1538ad8d8ad8c4aa771efe
Pars41 <ysfayds41@gmail.com>
2023-01-03 15:19:51 +0300

    palindrom

--- JavaScript/arrays/arrays1.js
+++ JavaScript/arrays/arrays1.js
@@ -1,23 +1,23 @@
-// //Dizi tanumlama
+//Dizi tanumlama
 
-// //1.Yöntem (Array literal) - Tercih edilen yöntem
+//1.Yöntem (Array literal) - Tercih edilen yöntem
 
-// const names = ["Ahmet","Furkan","Zeynep","Yusuf"];
-// console.log(names.length); 
-// console.log(typeof names);
-// names[3] = names[3].toUpperCase()
-// console.log(names);
+const names = ["Ahmet","Furkan","Zeynep","Yusuf"];
+console.log(names.length); 
+console.log(typeof names);
+names[3] = names[3].toUpperCase()
+console.log(names);
 
-// console.log(names.at(-1));   //son eleman -1 index
+console.log(names.at(-1));   //son eleman -1 index
 
-// //2.Yöntem (Object Constructor)
+//2.Yöntem (Object Constructor)
 
-// const codingLangs = new Array("C","C++","JS","GO") 
-// console.log(codingLangs);
+const codingLangs = new Array("C","C++","JS","GO") 
+console.log(codingLangs);
 
-// //3.Yöntem (Array.of())
-// const arr = Array.of(3)
-// console.log(arr);
+//3.Yöntem (Array.of())
+const arr = Array.of(3)
+console.log(arr);
 
 
 let cars = ["bmw","mercedes","tofas","renault","anadol"]
@@ -28,6 +28,7 @@ result = cars.unshift("citroen") // 6 ilk eleman olarak atar ve lenght i döndü
 result = cars.shift() // ilk elemanı siler ve döndürür
 result = cars.splice(1, 0, "vw") //
 result = cars.splice(3, 1, "toros") // start num,delete num ,parameter
+// result = cars.reverse()
 
 
 console.log(result);
---
+++ JavaScript/arrays/questions.js
@@ -0,0 +1,61 @@
+// Senior Level
+// ----------------------------------------------------
+// Find the closest palindrome number for your code:
+
+// Example:
+// Input: 216
+// Output: 212
+
+// Input: 100
+// Output: 99
+
+// Input: 77
+// Output: 77
+
+// NOTE:
+// - palindrome number is number is same from left - right side
+// - If the number itself is a palindrome, return that number.
+// - If two palindrome numbers distance is same choose smaller number.
+
+// let palindromeArray = [];
+// for (i=0; i<=1000; i++ ){
+//     if(i === Number( i.toString().split("").reverse().join(''))  ){
+//         palindromeArray.push(i)
+
+//     }
+// }
+// console.log(palindromeArray);
+
+// let i = 569;
+// console.log(i);
+// let sonuc = Number(i.toString().split("").reverse().join(''))
+// console.log(sonuc);
+
+function nearestPalindrom(numb) {
+  if (numb === Number(numb.toString().split("").reverse().join(""))) {
+    console.log(`${numb} is a palindrome number.`);
+  } else if (numb < 0) {
+    console.log("Nearest palindrome number is 0.");
+  } else {
+    let count = 0;
+    for (i = numb - 1; i > 0; i--) {
+      let pNearest = 0;
+      count++;
+      if (i === Number(i.toString().split("").reverse().join(""))) {
+        pNearest += i;
+        console.log(count);
+        break;
+      }
+    }
+    for (i = numb + 1; i < i + count; i++) {
+      if (i === Number(i.toString().split("").reverse().join(""))) {
+        pNearest = i;
+         break;
+      }
+    }
+    console.log(`Nearest palindrome is ${pNearest}`);
+  }
+}
+
+nearestPalindrom(10);
+
