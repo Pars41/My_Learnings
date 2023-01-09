0a33ca6debdafbad1a1052e3acaf47d07e9164c6
Pars41 <ysfayds41@gmail.com>
2023-01-09 22:55:22 +0300

    destructure

--- JavaScript/objects/newOps.js
+++ JavaScript/objects/newOps.js
@@ -1,35 +1,35 @@
-//* EXAMPLE: NESTED
+// //* EXAMPLE: NESTED
 
-const cars = {
-  car1: {
-    name: "BMW",
-    model: 1990,
-    engine: 1.6,
-  },
-  car2: {
-    name: "Mercedes",
-    model: 2022,
-    engine: 2.0,
-  },
-};
+// const cars = {
+//   car1: {
+//     name: "BMW",
+//     model: 1990,
+//     engine: 1.6,
+//   },
+//   car2: {
+//     name: "Mercedes",
+//     model: 2022,
+//     engine: 2.0,
+//   },
+// };
 
-const { car1, car2 } = cars;
+// const { car1, car2 } = cars;
 
-// console.log(car1,car2);
+// // console.log(car1,car2);
 
-const { name: nameCar1, engine: engineCar1 } = car1;
-const { name: nameCar2, engine: engineCar2 } = car2;
-console.log(nameCar1, engineCar2);
+// const { name: nameCar1, engine: engineCar1 } = car1;
+// const { name: nameCar2, engine: engineCar2 } = car2;
+// console.log(nameCar1, engineCar2);
 
-const {
-  car1: { name },
-  car2: { model },
-} = cars;
+// const {
+//   car1: { name },
+//   car2: { model },
+// } = cars;
 
-console.log(name, model);
+// console.log(name, model);
 
-const model2 = cars.car1.model;
-console.log(model2);
+// const model2 = cars.car1.model;
+// console.log(model2);
 
 //Example
 const team = [
@@ -55,10 +55,21 @@ const team = [
 
 //classical
 
-team.forEach(t=>{
-    console.log("NAME:",t.name);
+team.forEach((t,i)=>{
+    console.log(i+1+"."+"NAME:",t.name);
     console.log("SURNAME:",t.surname);
     console.log("JOB:",t.job);
     console.log("AGE:",t.age);
+    console.log("***********");
+})
+
+//destructure
+
+team.forEach((person,i)=>{
+    const {name,surname,job,age } = person
+    console.log(i+1+"."+"NAME:",name);
+    console.log("SURNAME:",surname);
+    console.log("JOB:",job);
+    console.log("AGE:",age);
     console.log("***********");
 })
