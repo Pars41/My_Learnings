9de72c589d03cfc6d46ab9aa1493a4ee65e94809
Yusuf <ysfayds41@gmail.com>
2023-01-11 20:22:24 +0300

    destruc.

--- JavaScript/objects/newOps.js
+++ JavaScript/objects/newOps.js
@@ -84,3 +84,14 @@ const getInfo = () => {
 }
 const {productname,ids} = getInfo()
 console.log(ids);
+
+//* ======================================================
+//*  DESTRUCTURING (ARRAY)
+//* ======================================================
+const names = ["Ahmet", "Mehmet", "İsmet", "Saffet"]
+
+//*Classical
+const mehmet = names[1] //* indexing
+
+const [p1, p2, , p4] = names
+console.log(p1, p2, p4)
