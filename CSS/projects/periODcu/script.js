96eda445e303a3ce63f965752ab3d7da51b3e854
Yusuf <ysfayds41@gmail.com>
2023-01-15 20:34:22 +0300

    periodcu

--- CSS/projects/periODcu/script.js
+++ CSS/projects/periODcu/script.js
@@ -1,5 +1,1 @@
-let arama = document.getElementById("atom")
-console.log(arama)
-let hulk = arama.ariaValueText === 1
-console.log(hulk)
-console.log(arama)
+document.getElementById("pink").onmousemove
