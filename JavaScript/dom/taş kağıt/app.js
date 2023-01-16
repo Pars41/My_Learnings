8b5cb570b0d575c57e8ad42c19da16f0eef5b224
Yusuf <ysfayds41@gmail.com>
2023-01-16 20:26:20 +0300

    game

---
+++ JavaScript/dom/taş kağıt/app.js
@@ -0,0 +1,16 @@
+const yourChoice = document.getElementById("your-choice")
+
+// const tasImage = document.querySelector(".tas")
+// const kagitImage = document.querySelector(".kagit")
+// const makasImage = document.querySelector(".makas")
+
+
+// tasImage.addEventListener('click',()=>{
+//     yourChoice.innerHTML = '<img src="./assets/tas.png"></img>'
+// })
+// kagitImage.addEventListener('click',()=>{
+//     yourChoice.innerHTML = '<img src="./assets/kagit.png"></img>'
+// })
+// makasImage.addEventListener('click',()=>{
+//     yourChoice.innerHTML = '<img src="./assets/makas.png"></img>'
+// })
