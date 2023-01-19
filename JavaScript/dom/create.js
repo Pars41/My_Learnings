3131417c1e56200e22da15057d5a517e9303143e
Yusuf <ysfayds41@gmail.com>
2023-01-19 22:30:43 +0300

    a

--- JavaScript/dom/create.js
+++ JavaScript/dom/create.js
@@ -1,15 +1,31 @@
-document.getElementById("horhor").style.color = "blueviolet"
-const altp = document.getElementById("horhor").nextSibling.nextSibling
+// document.getElementById("horhor").style.color = "blueviolet"
+// const altp = document.getElementById("horhor").nextSibling.nextSibling
 
-const newPar = document.createElement("p")
-const text = document.createTextNode("Bu bir p elementidir")
-newPar.appendChild(text)
+// const newPar = document.createElement("p")
+// const text = document.createTextNode("Bu bir p elementidir")
+// newPar.appendChild(text)
 
-const h4 = document.querySelector("h4")
-h4.after(newPar)
+// const h4 = document.querySelector("h4")
+// h4.after(newPar)
 
-altp.style.color = "tomato"
-altp.className = "center"
-document.querySelector(".center").style.color = "yellow"
-const yusuf = altp.nextSibling.textContent.split("")
-console.log(yusuf[7].text)
+// altp.style.color = "tomato"
+// altp.className = "center"
+// document.querySelector(".center").style.color = "yellow"
+// const yusuf = altp.nextSibling.textContent.split("")
+// console.log(yusuf[7].text)
+
+// let x= 5;
+// let y= 6;
+// console.log(Math.min(x,y)) 
+
+// const notifications = 1;
+// console.log(
+// `You have ${notifications} notification${notifications > 1 ? 's' : ''}`
+// );
+
+const instructors = {
+    name: 'Noah',
+    age: 12
+    };
+    delete instructors.name;
+    console.log(instructors)
