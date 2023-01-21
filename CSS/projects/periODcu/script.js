baf246ec4212abea3b54d8c1b13087ef04db46ef
Yusuf <ysfayds41@gmail.com>
2023-01-21 19:42:52 +0300

    periodcu

--- CSS/projects/periODcu/script.js
+++ CSS/projects/periODcu/script.js
@@ -1,1 +1,17 @@
-document.getElementById("pink").onmousemove
+let spaN = document.querySelectorAll(".numb")
+let alkali = document.getElementById("alkali")
+alkali.addEventListener("click",function(){
+    spaN.forEach((e)=>{
+        if(e.innerHTML != 3  && e.innerHTML != 11 && e.innerHTML != 19 && e.innerHTML != 37 && e.innerHTML != 55 && e.innerHTML != 87){
+         e.parentElement.classList.toggle("silik")
+        }
+         
+     })
+})
+let div = document.querySelectorAll("div")
+
+div.forEach((e)=>{
+    e.addEventListener("click",function(e){
+        e.target.classList.add("bigger")
+    })
+})
