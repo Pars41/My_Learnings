0b243d184239412209aa8abc3473468269ea52a8
Yusuf <ysfayds41@gmail.com>
2023-01-28 15:03:22 +0300

    api fetch

--- JavaScript/async-await/script.js
+++ JavaScript/async-await/script.js
@@ -3,7 +3,7 @@
 //.env (environment)  -gitignore
 const getNews = async () => {
   const API_KEY = "b3910268dce8481caef14e805a9a95c5";
-  const URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+  const URL = `https://newsapi.org/v2/top-headlines?country=tr&apiKey=${API_KEY}`;
   try {
     const res = await fetch(URL);
     if (!res.ok) {
@@ -21,13 +21,15 @@ const renderNews = (news) => {
     const {title,description,urlToImage,url} = item
 
     newsDiv.innerHTML += `
-  <div class="card" style="width: 18rem;">
+    <div class="card col-md-6 col-lg-4 col-xl-3" >
+  <div class="card" >
         <img src="${urlToImage}" class="card-img-top" alt="...">
         <div class="card-body">
              <h5 class="card-title">${title}</h5>
               <p class="card-text">${description}</p>
-              <a href="${url}" target="_blank" class="btn btn-primary">Go somewhere</a>
+              <a href="${url}" target="_blank" class="btn btn-primary">Habere Git</a>
   </div>
+</div>
 </div>`
     
   })
